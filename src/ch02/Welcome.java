553c0a18e8d354d04b9fd860f39c606f351f1a9d
oraman <oraman@naver.com>
2023-05-31 11:01:27 +0900

    pull하지 않은 데이터를 push 할려고 시도합니다.

--- src/ch02/Welcome.java
+++ src/ch02/Welcome.java
@@ -3,7 +3,6 @@ package ch02;
 public class Welcome {
     public static void main(String[] args) {
         System.out.println("welcome01");
-        // 현재 최신 Welcome 파일을 pull하지 않았습니다.
-        System.out.println("welcome02_written by hee");
+        System.out.println("welcome02");
     }
 }
